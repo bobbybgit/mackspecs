b6882eb796e87ade731b7138f315c3c5eb911bd8
bobbybgit <120116335+bobbybgit@users.noreply.github.com>
2023-05-05 19:37:12 +0000

    basic layout setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+     # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
+  protected
+  
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :surname, :user_image])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :surname, :user_image])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :parts
   resources :specs
   devise_for :users
+  devise_scope :user do
+    match '/sessions/user', to: 'devise/sessions#create', via: :post
+  end
+
   get 'pages/dash'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
