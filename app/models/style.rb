96258ff48632af7e592ee24421fea816580c09d0
bobbybgit <120116335+bobbybgit@users.noreply.github.com>
2023-05-15 15:01:59 +0000

    Changed brands to be stored as code for updates

--- app/models/style.rb
+++ app/models/style.rb
@@ -5,7 +5,19 @@ class Style < ApplicationRecord
   scope :style_search, -> (search_string){where('name LIKE ? OR style_num LIKE ? OR brand LIKE ?', "%#{search_string}%", "%#{search_string}%", "%#{search_string}%")}
 
   def self.brands
-    ['Mackintosh (ROW)', 'Mackintosh (Japan)'] + ['Kiko Kostadinov', 'Aether', 'The Workers Club','This Thing of Ours','The Power For The People x','Nigel Cabourn','Carhartt'].sort
+    ['Kiko Kostadinov', 'Aether', 'The Workers Club','This Thing of Ours','The Power For The People x','Nigel Cabourn','Carhartt']
+  end
+
+  def self.priority_brands
+    ['Mackintosh ROW', 'Mackintosh Japan']
+  end
+
+  def self.grouped_brands_for_select
+    [["",[]],["----------", Style.priority_brands.map.with_index{|brand,i|[brand,"P#{i}"]}],["----------", Style.brands.map.with_index{|brand,i|[brand,i]}.sort],["",[]]]
+  end
+
+  def self.interpret_brand(id)
+    id[0] == "P" ? Style.priority_brands[id[1..-1].to_i] : Style.brands[id.to_i]
   end
 
 end
